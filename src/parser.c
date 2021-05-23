f53ab02079e629fba28e2030d934a385a8b44e2f
Tomasz Ziębowicz <tz429707@students.mimuw.edu.pl>
2021-05-24 01:01:56 +0200

    Update CMakeLists.txt, create parser class

---
+++ src/parser.c
@@ -0,0 +1,5 @@
+//
+// Created by tmk on 24.05.2021.
+//
+
+#include "parser.h"
---
+++ src/parser.h
@@ -0,0 +1,8 @@
+//
+// Created by tmk on 24.05.2021.
+//
+
+#ifndef POLYNOMIALS_PARSER_H
+#define POLYNOMIALS_PARSER_H
+
+#endif // POLYNOMIALS_PARSER_H
